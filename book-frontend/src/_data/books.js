3a600d778783a728d5bc4ec88cbcec3af579e22b
shaik.karishma <karishmaptp@gmail.com>
2026-05-20 13:01:51 +0200

    learned the filter concept in the eleventy and used it

--- book-frontend/src/_data/books.js
+++ book-frontend/src/_data/books.js
@@ -1,6 +1,6 @@
 export default async function () {
   const res = await fetch(
-    "http://localhost:1337/api/books?populate=category"
+    "http://localhost:1337/api/books?populate=*"
   );
 
   const json = await res.json();
